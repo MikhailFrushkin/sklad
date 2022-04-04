7e06ddcbd2ce441e7bca4456c586098f9e92a33f
Mikhail Fryshkin <muxazila@mail.ru>
2022-04-04 23:17:10 +0700

    213

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,5 +1,6 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
+import qrcode
 
 import bot
 from keyboards.default import menu
@@ -20,6 +21,19 @@ async def bot_message(message: types.Message):
     if message.text == 'VSL':
         await bot.send_message(message.from_user.id, 'vls')
     elif message.text == 'Brak':
-        await  bot.send_message(message.from_user.id, 'brak')
+        await bot.send_message(message.from_user.id, 'brak')
     else:
-        await bot.send_message(message.from_user.id, 'sadsadsad')
+        await bot.send_message(message.from_user.id, '{}'.format(message.text))
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data('{}'.format(message.text))
+        qr.make(fit=True)
+
+        img = qr.make_image(fill_color="black", back_color="white")
+        img.save('{}.jpg'.format(message.text), 'JPEG')
+        qrc = open('{}.jpg'.format(message.text), 'rb')
+        await bot.send_photo(message.chat.id, qrc)
