a68609b187a5389ab5840118b4c3479dc950afe5
Mikhail Fryshkin <muxazila@mail.ru>
2022-04-04 23:34:44 +0700

    213

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -20,20 +20,31 @@ async def bot_message(message: types.Message):
 
     if message.text == 'VSL':
         await bot.send_message(message.from_user.id, 'vls')
+
+        qrc = open('V_Sales-825.jpg', 'rb')
+        await bot.send_photo(message.chat.id, qrc)
+
     elif message.text == 'Brak':
-        await bot.send_message(message.from_user.id, 'brak')
+        await bot.send_message(message.from_user.id, 'Доделаю завтра')
+
+
     else:
         await bot.send_message(message.from_user.id, '{}'.format(message.text))
-        qr = qrcode.QRCode(
-            version=1,
-            error_correction=qrcode.constants.ERROR_CORRECT_L,
-            box_size=10,
-            border=4,
-        )
-        qr.add_data('{}'.format(message.text))
-        qr.make(fit=True)
 
-        img = qr.make_image(fill_color="black", back_color="white")
-        img.save('{}.jpg'.format(message.text), 'JPEG')
-        qrc = open('{}.jpg'.format(message.text), 'rb')
-        await bot.send_photo(message.chat.id, qrc)
+        str_answer = message.text
+        if not len(str_answer) == 3:
+            await bot.send_message(message.from_user.id, 'Неверно указана ячейка!')
+        else:
+            qr = qrcode.QRCode(
+                version=1,
+                error_correction=qrcode.constants.ERROR_CORRECT_L,
+                box_size=10,
+                border=4,
+            )
+            qr.add_data('012_825-0{}-0{}-{}'.format(message.text[0], message.text[1], message.text[2]))
+            qr.make(fit=True)
+
+            img = qr.make_image(fill_color="black", back_color="white")
+            img.save('{}.jpg'.format(message.text), 'JPEG')
+            qrc = open('{}.jpg'.format(message.text), 'rb')
+            await bot.send_photo(message.chat.id, qrc)
