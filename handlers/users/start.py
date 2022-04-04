b529ab585869bb8d873f218ee5230a181cb94015
Mikhail Fryshkin <muxazila@mail.ru>
2022-04-05 00:18:15 +0700

    213

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -28,47 +28,50 @@ async def bot_message(message: types.Message):
         await bot.send_message(message.from_user.id, 'Доделаю когда-нибудь -)')
     else:
         ans = message.text
-        if len(ans) == 3:
-            if int(ans[1]) < 9 and int(ans[2]) < 5:
+        if ans.isdigit():
+            if len(ans) == 3:
+                if int(ans[1]) < 9 and int(ans[2]) < 5:
 
-                await bot.send_message(message.from_user.id, '{} ряд {} секция {} ячейка'.
-                                       format(ans[0], ans[1], ans[2]))
+                    await bot.send_message(message.from_user.id, '{} ряд {} секция {} ячейка'.
+                                           format(ans[0], ans[1], ans[2]))
 
-                qr = qrcode.QRCode(
-                    version=1,
-                    error_correction=qrcode.constants.ERROR_CORRECT_L,
-                    box_size=10,
-                    border=4,
-                )
-                qr.add_data('012_825-0{}-0{}-{}'.format(message.text[0], message.text[1], message.text[2]))
-                qr.make(fit=True)
+                    qr = qrcode.QRCode(
+                        version=1,
+                        error_correction=qrcode.constants.ERROR_CORRECT_L,
+                        box_size=10,
+                        border=4,
+                    )
+                    qr.add_data('012_825-0{}-0{}-{}'.format(message.text[0], message.text[1], message.text[2]))
+                    qr.make(fit=True)
 
-                img = qr.make_image(fill_color="black", back_color="white")
-                img.save('qcodes/{}.jpg'.format(message.text), 'JPEG')
-                qrc = open('qcodes/{}.jpg'.format(message.text), 'rb')
-                await bot.send_photo(message.chat.id, qrc)
-            else:
-                await bot.send_message(message.from_user.id, 'Неверно указана ячейка!')
-        elif len(ans) == 4 and int(ans[0]) == 1:
-            if int(ans[2]) < 9 and int(ans[3]) < 5:
+                    img = qr.make_image(fill_color="black", back_color="white")
+                    img.save('qcodes/{}.jpg'.format(message.text), 'JPEG')
+                    qrc = open('qcodes/{}.jpg'.format(message.text), 'rb')
+                    await bot.send_photo(message.chat.id, qrc)
+                else:
+                    await bot.send_message(message.from_user.id, 'Неверно указана ячейка!')
+            elif len(ans) == 4 and int(ans[0]) == 1:
+                if int(ans[2]) < 9 and int(ans[3]) < 5:
 
-                await bot.send_message(message.from_user.id, '{}{} ряд {} секция {} ячейка'.
-                                       format(ans[0], ans[1], ans[2], ans[3]))
+                    await bot.send_message(message.from_user.id, '{}{} ряд {} секция {} ячейка'.
+                                           format(ans[0], ans[1], ans[2], ans[3]))
 
-                qr = qrcode.QRCode(
-                    version=1,
-                    error_correction=qrcode.constants.ERROR_CORRECT_L,
-                    box_size=10,
-                    border=4,
-                )
-                qr.add_data('012_825-0{}{}-0{}-{}'
-                            .format(message.text[0], message.text[1], message.text[2], message.text[3]))
-                qr.make(fit=True)
-                img = qr.make_image(fill_color="black", back_color="white")
-                img.save('qcodes/{}.jpg'.format(message.text), 'JPEG')
-                qrc = open('qcodes/{}.jpg'.format(message.text), 'rb')
-                await bot.send_photo(message.chat.id, qrc)
+                    qr = qrcode.QRCode(
+                        version=1,
+                        error_correction=qrcode.constants.ERROR_CORRECT_L,
+                        box_size=10,
+                        border=4,
+                    )
+                    qr.add_data('012_825-0{}{}-0{}-{}'
+                                .format(message.text[0], message.text[1], message.text[2], message.text[3]))
+                    qr.make(fit=True)
+                    img = qr.make_image(fill_color="black", back_color="white")
+                    img.save('qcodes/{}.jpg'.format(message.text), 'JPEG')
+                    qrc = open('qcodes/{}.jpg'.format(message.text), 'rb')
+                    await bot.send_photo(message.chat.id, qrc)
+                else:
+                    await bot.send_message(message.from_user.id, 'Неверно указана ячейка!')
             else:
                 await bot.send_message(message.from_user.id, 'Неверно указана ячейка!')
         else:
-            await bot.send_message(message.from_user.id, 'Неверно указана ячейка!')
+            await bot.send_message(message.from_user.id, 'Многа букафф')
