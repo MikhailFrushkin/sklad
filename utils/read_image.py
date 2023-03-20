0464987327b4be61be30db5425a33d3fcaea2ba3
mikhail <muxazila@mail.ru>
2023-03-20 13:34:00 +0700

    read photo upgrade

--- utils/read_image.py
+++ utils/read_image.py
@@ -21,21 +21,6 @@ def read_image(img: str) -> list:
         rotated = cv2.warpAffine(img, M, (w, h))
         cv2.imwrite(f'{path}/photos/фото_{angle}.png', rotated)
 
-        hsv_img = cv2.cvtColor(rotated, cv2.COLOR_BGR2HSV)
-        # определите диапазон красного цвета в HSV
-        lower_red = np.array([0, 50, 50])
-        upper_red = np.array([10, 255, 255])
-        mask1 = cv2.inRange(hsv_img, lower_red, upper_red)
-        lower_red = np.array([170, 50, 50])
-        upper_red = np.array([180, 255, 255])
-        mask2 = cv2.inRange(hsv_img, lower_red, upper_red)
-
-        # Объединить две маски красного цвета в одну
-        mask = mask1 + mask2
-        masked_img = cv2.bitwise_and(rotated, rotated, mask=mask)
-        red_text = cv2.cvtColor(masked_img, cv2.COLOR_HSV2BGR)
-        cv2.imwrite(f'result{angle}.png', red_text)
-
         config = r'--oem 1 --psm 6'
         text = pytesseract.image_to_string(rotated, config=config, lang='eng')
 
@@ -45,6 +30,20 @@ def read_image(img: str) -> list:
         if result:
             print(result)
         else:
+            hsv_img = cv2.cvtColor(rotated, cv2.COLOR_BGR2HSV)
+            # определите диапазон красного цвета в HSV
+            lower_red = np.array([0, 50, 50])
+            upper_red = np.array([10, 255, 255])
+            mask1 = cv2.inRange(hsv_img, lower_red, upper_red)
+            lower_red = np.array([170, 50, 50])
+            upper_red = np.array([180, 255, 255])
+            mask2 = cv2.inRange(hsv_img, lower_red, upper_red)
+
+            # Объединить две маски красного цвета в одну
+            mask = mask1 + mask2
+            masked_img = cv2.bitwise_and(rotated, rotated, mask=mask)
+            red_text = cv2.cvtColor(masked_img, cv2.COLOR_HSV2BGR)
+            cv2.imwrite(f'result{angle}.png', red_text)
             text = pytesseract.image_to_string(red_text, config=config, lang='eng')
             pattern = re.compile(r"\b\d{8}\b")
             result = re.findall(pattern, text)
